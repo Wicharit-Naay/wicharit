a894ef745c3ea0f73cadbc074e15c2fb89f51176
Wicharit-Naay <https://github.com/Wicharit-Naay/wicharit.git>
2026-02-10 12:02:25 +0700

    a

---
+++ i/b.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>วิชาฤทธิ์ ร้อยคำลือ (นะนาย)</title>
+</head>
+<body>
+    <h1>ข้อมูลจังหวัด -- วิชาฤทธิ์ ร้อยคำลือ (นะนาย)</h1>
+
+    <form method="post" action="">
+    ชื่อจังหวัด <input type="text" name="rname" autofocus required> <br>
+    รูปภาพ <input type="file" name="piage"><br>
+    ชื่อภาค
+    <select name = "rid">
+    <?php
+    include_once("connectdb.php");
+    $sql3 ="SELECT * FROM `regions` ORDER BY`r_name` ASC";
+    $rs3 = mysqli_query($conn,$sql3);
+    while($data3 = mysqli_fetch_array($rs3)){
+?>
+<option value="<?php echo $data3['r_id'];?>"><?php echo $data3['r_name'];?></option>
+<?php } ?>
+</select><br><br>
+    <button type="submit" name="Submit">บันทึก</button>
+</form>
+<br>
+<br>
+
+<?php
+if(isset($_POST['Submit'])){
+    //include_once("connectdb.php");
+    //$rname = $_POST['rname'];
+    //$sql2 ="INSERT INTO `regions` VALUES (NULL, '{$rname}')";
+    //mysqli_query($conn,$sql2) or dir ("insert ไม่ได้");
+}
+?>
+
+    <table border="1">
+        <tr>
+            <th>รหัส</th>
+            <th>ชื่อจังหวัด</th>
+            <th>รูปภาพ</th>
+            <th>ภาค</th>
+        </tr>
+
+        <?php
+        include_once("connectdb.php");
+        $sql = "SELECT * FROM `provinces` AS p
+        INNER JOIN `regions` AS r
+        ON p.r_id = r.r_id
+        ORDER BY `p_name` ASC";
+        $rs = mysqli_query($conn,$sql);
+        while($data = mysqli_fetch_array($rs)){
+    ?>
+
+        <tr>
+            <td><?php echo $data['p_id'];?></td>
+            <td><?php echo $data['p_name'];?></td>
+            <td><img src="imgs/<?php echo $data['p_id'];?>.<?php echo $data['p_ext'];?>" width="120"></td>
+            <td><?php echo $data['r_name'];?></td>
+        </tr>
+
+        <?php }?>
+    </table>
+
+</body>
+</html>
