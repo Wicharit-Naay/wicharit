f0dcbe720cbeda1bd2c20ca8e1d92e86a9ec6910
Wicharit-Naay <https://github.com/Wicharit-Naay/wicharit.git>
2026-02-10 09:52:27 +0700

    Add folder i

---
+++ i/a.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>วิชาฤทธิ์ ร้อยคำลือ (นะนาย)</title>
+</head>
+<body>
+    <h1>งาน i</h1>
+    <h1>วิชาฤทธิ์ ร้อยคำลือ (นะนาย)</h1>
+</body>
+</html>
